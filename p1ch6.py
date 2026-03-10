70800805c4ecf5882393ce4823c985b67508fdad
Elix136 <soin.ilya.konstant@gmail.com>
2026-03-10 09:16:14 +0300

    Sequental

--- p1ch6.py
+++ p1ch6.py
@@ -6,8 +6,10 @@ t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
 t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 t_c = torch.tensor(t_c).unsqueeze(1)
 t_u = torch.tensor(t_u).unsqueeze(1)
-t_un = t_u *0.1
-linear_model = nn.Linear(1, 1)
+t_un = t_u * 0.1
+linear_model = nn.Sequential(nn.Linear(1, 13),
+                             nn.Tanh(),
+                             nn.Linear(13,1))
 optimizer = optim.SGD(linear_model.parameters(), lr = 1e-2)
 
 def training_loop(n_epohs, optimizer,model, loss_fn, t_u, t_c):
@@ -20,12 +22,12 @@ def training_loop(n_epohs, optimizer,model, loss_fn, t_u, t_c):
         optimizer.step()
         print(loss.item())
 
-training_loop(n_epohs=1000, 
+training_loop(n_epohs=5000, 
               optimizer=optimizer, 
               model=linear_model, 
               loss_fn=nn.MSELoss(), 
               t_u=t_un, 
               t_c= t_c)
 
-print(linear_model.weight, linear_model.bias)
+print(linear_model(t_u))
 
