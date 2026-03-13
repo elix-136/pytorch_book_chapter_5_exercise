221e77655d117a867c3c3cbacb44d83b7f5a55e6
elix-136 <il21994@yandex.ru>
2026-03-13 22:58:25 +0300

    Normalize

--- p1ch7.py
+++ p1ch7.py
@@ -15,4 +15,20 @@ cifar10_val = datasets.CIFAR10(data_path,
                                transform = transforms.ToTensor())
 
 imgs = torch.stack([img_t for img_t, _ in cifar10], dim=3)
-print(imgs.shape)
+
+mean = imgs.view(3,-1).mean(dim=1)
+std = imgs.view(3,-1).std(dim=1)
+
+t_cifar10 = datasets.CIFAR10(data_path, 
+                           train = True, 
+                           download = True,
+                           transform = transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((imgs.view(3,-1).mean(dim=1)),
+                                                    (imgs.view(3,-1).std(dim=1)))    
+                            ]
+                           ))
+
+img_t, _ = t_cifar10[99]
+plt.imshow(img_t.permute(1,2,0))
+plt.show()
